dffc12bf97928a686a6d7f42ce8984fe0ce7e7a1
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-03 12:22:04 +0400

    Add sequenceOfCreationDefault benchmark

--- src/commonMain/kotlin/singlesequenceof/SequenceOperationsBenchmark.kt
+++ src/commonMain/kotlin/singlesequenceof/SequenceOperationsBenchmark.kt
@@ -1,14 +1,26 @@
 package singlesequenceof
 
 import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Blackhole
+import kotlinx.benchmark.Param
 import kotlinx.benchmark.Scope
 import kotlinx.benchmark.State
 
 @State(Scope.Benchmark)
 class SequenceOperationsBenchmark {
 
+    // Basic creation benchmarks
     @Benchmark
-    fun benchmarkMethod(): Int {
-        return 1 + 1
+    fun sequenceOfCreationDefault(blackhole: Blackhole, state: CreationState) {
+        repeat(state.count) {
+            val seq = sequenceOf(1)
+            blackhole.consume(seq)
+        }
+    }
+
+    @State(Scope.Benchmark)
+    class CreationState {
+        @Param("1", "10", "1000", "1000000")
+        var count: Int = 0
     }
 }
