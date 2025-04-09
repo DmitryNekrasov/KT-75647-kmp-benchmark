c9d65bb59cdf845a7cdc5baebfe28a7718ec6237
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-09 11:41:21 +0400

    Remove outer loop for SequenceBasicCreationBenchmark

--- src/commonMain/kotlin/singlesequenceof/SequenceBasicCreationBenchmark.kt
+++ src/commonMain/kotlin/singlesequenceof/SequenceBasicCreationBenchmark.kt
@@ -2,45 +2,33 @@ package singlesequenceof
 
 import kotlinx.benchmark.Benchmark
 import kotlinx.benchmark.Blackhole
-import kotlinx.benchmark.Param
 import kotlinx.benchmark.Scope
 import kotlinx.benchmark.State
 
 @State(Scope.Benchmark)
 class SequenceBasicCreationBenchmark {
 
-    @Param("1", "10", "1000", "1000000")
-    var creationCount: Int = 0
-
     @Benchmark
     fun singleSequenceOfCreationDefault(blackhole: Blackhole) {
-        repeat(creationCount) {
-            val seq = sequenceOf(1)
-            blackhole.consume(seq)
-        }
+        val seq = sequenceOf(1)
+        blackhole.consume(seq)
     }
 
     @Benchmark
     fun singleSequenceOfCreationSingle(blackhole: Blackhole) {
-        repeat(creationCount) {
-            val seq = singleSequenceOf(1)
-            blackhole.consume(seq)
-        }
+        val seq = singleSequenceOf(1)
+        blackhole.consume(seq)
     }
 
     @Benchmark
     fun emptySequenceOfCreationDefault(blackhole: Blackhole) {
-        repeat(creationCount) {
-            val seq = sequenceOf<Int>()
-            blackhole.consume(seq)
-        }
+        val seq = sequenceOf<Int>()
+        blackhole.consume(seq)
     }
 
     @Benchmark
     fun emptySequenceOfCreationSingle(blackhole: Blackhole) {
-        repeat(creationCount) {
-            val seq = emptySequenceOf<Int>()
-            blackhole.consume(seq)
-        }
+        val seq = emptySequenceOf<Int>()
+        blackhole.consume(seq)
     }
 }
